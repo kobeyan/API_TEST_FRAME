6471cfc15375da2178646f2fd1c09f3f56b83a87
kobeyan <18670335072@163.com>
2020-07-02 23:21:42 +0800

    读取excel

---
+++ samples/homework_read_excel.py
@@ -0,0 +1,9 @@
+
+import os
+import xlrd
+
+
+# excel_path = os.path.join(os.path.dirname(os.path.abspath( __file__)))
+excel_path = os.path.join(os.path.dirname(os.path.abspath( __file__)),"data\\test_data.xlsx")
+
+print(excel_path)
